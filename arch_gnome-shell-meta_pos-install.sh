4a604672d510f63ea2c961d859cafe24e3a6fd92
elppans <elppansmk@gmail.com>
2025-03-09 04:44:17 -0300

    Update arch_gnome-shell-meta_pos-install.sh

--- arch_gnome-shell-meta_pos-install.sh
+++ arch_gnome-shell-meta_pos-install.sh
@@ -13,7 +13,7 @@ sudo pkgfile -u
 # Wrappers do pacman (AUR Helper)
 git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin
 cd /tmp/paru-bin
-makepkg -Cris
+makepkg --needed --noconfirm -Cris
 
 # Remoção de aplicativos
 sudo pacman --noconfirm -R gnome-music loupe
