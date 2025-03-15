4b4e262bce0024ab62d25eb9761413e37323d5bb
elppans <elppans@gmail.com>
2025-03-15 01:32:47 -0300

    Adicionado dependência game-devices-udev para Stem Flatpak

--- pacote-flatpak-steam.sh
+++ pacote-flatpak-steam.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Instala o pacote game-devices-udev, que fornece regras udev para dispositivos de jogo (como controle, joystick, etc.)  
+# Dependẽncia para Steam versão Flatpak
+paru --needed --noconfirm -S game-devices-udev
+
 # Instala o Steam (plataforma de jogos) via Flatpak a partir do repositório Flathub  
 sudo flatpak install -y flathub com.valvesoftware.Steam
 
