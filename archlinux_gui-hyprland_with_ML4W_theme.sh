2ab0bc02f653e7844639a456965341272f2b2f0e
elppans <elppans@gmail.com>
2025-03-22 18:15:37 -0300

    Adicionado instalação de hyprutils nwg-displays

--- archlinux_gui-hyprland_with_ML4W_theme.sh
+++ archlinux_gui-hyprland_with_ML4W_theme.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# https://github.com/mylinuxforwork/dotfiles/wiki
 # https://github.com/mylinuxforwork/dotfiles?tab=readme-ov-file
 # https://github.com/mylinuxforwork/dotfiles/wiki/Installation
+# https://github.com/mylinuxforwork/dotfiles/wiki/Troubleshooting
+# https://github.com/mylinuxforwork/dotfiles/wiki/Monitor-Configuration
+
 
 # if pacman -Qqs hyprland ; then
     # Pacotes essenciais para desenvolvimento
@@ -11,8 +15,11 @@
     cd /tmp/ml4w-hyprland || exit 1
     makepkg --needed --noconfirm -Cris && \
     ml4w-hyprland-setup
+    sudo pacman --needed --noconfirm -S hyprutils nwg-displays
 # else
 # 	echo "Deve instalar a base Hyprland primeiro!"
 # 	exit 1
 # fi
 
+# Se não quiser usar o Dock, crie este arquivo:
+# touch $HOME/.config/ml4w/settings/dock-disabled
