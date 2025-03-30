d7c9f9183f6304475bfda534679ca9bfc6086a19
elppans <elppans@gmail.com>
2025-03-30 13:44:13 -0300

    Atualizado chaotic-aur

--- pacman_aur/repositorio-chaotic-aur.sh
+++ pacman_aur/repositorio-chaotic-aur.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-echo "Deseja ativar o repositório chaotic-aur? (y/N) [N padrão]"
+# Cores
+VERDE="\033[0;32m"
+VERMELHO="\033[0;31m"
+AZUL="\033[0;34m"
+AMARELO="\033[0;33m"
+PADRAO="\033[0m"
+
+echo -e "${AZUL}Deseja ativar o repositório chaotic-aur? (y/n) [n padrão]${PADRAO}"
 read -r resposta
 
 # Define 'n' como padrão se ENTER for pressionado sem entrada
 resposta=${resposta:-n}
 
 if [[ $resposta == "y" || $resposta == "Y" ]]; then
-    echo "Ativando o repositório chaotic-aur..."
+    echo -e "${VERDE}Ativando o repositório chaotic-aur...${PADRAO}"
     curl -JLk -o /usr/local/bin/chaotic-aur https://raw.githubusercontent.com/HyDE-Project/HyDE/refs/heads/master/Scripts/chaotic_aur.sh
     sudo chmod +x /usr/local/bin/chaotic-aur
-    sudo chaotic-aur --install
-    echo "Repositório instalado com sucesso!"
+    sudo /usr/local/bin/chaotic-aur --install
+    echo -e "${VERDE}Repositório instalado com sucesso!${PADRAO}"
 elif [[ $resposta == "n" || $resposta == "N" ]]; then
-    echo "O repositório chaotic-aur não foi ativado."
+    echo -e "${AMARELO}O repositório chaotic-aur não foi ativado.${PADRAO}"
 else
-    echo "Opção inválida. Por favor, responda com 'y' ou 'n'."
+    # echo -e "${VERMELHO}Opção inválida. Por favor, responda com 'y' ou 'n'.${PADRAO}"
+    echo -e "${VERMELHO}O repositório chaotic-aur não será ativado.${PADRAO}"
 fi
