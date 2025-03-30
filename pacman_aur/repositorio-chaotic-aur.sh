2b275a16c81e944be5ea094a5b77cb268d5d18d6
elppans <elppans@gmail.com>
2025-03-30 13:37:25 -0300

    Atualizado Script chaotic-aur

--- pacman_aur/repositorio-chaotic-aur.sh
+++ pacman_aur/repositorio-chaotic-aur.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-https://raw.githubusercontent.com/HyDE-Project/HyDE/refs/heads/master/Scripts/chaotic_aur.sh
-sudo chmod +x /usr/local/bin/chaotic-aur 
-sudo chaotic-aur --install
+echo "Deseja ativar o repositório chaotic-aur? (y/N) [N padrão]"
+read -r resposta
+
+# Define 'n' como padrão se ENTER for pressionado sem entrada
+resposta=${resposta:-n}
+
+if [[ $resposta == "y" || $resposta == "Y" ]]; then
+    echo "Ativando o repositório chaotic-aur..."
+    curl -JLk -o /usr/local/bin/chaotic-aur https://raw.githubusercontent.com/HyDE-Project/HyDE/refs/heads/master/Scripts/chaotic_aur.sh
+    sudo chmod +x /usr/local/bin/chaotic-aur
+    sudo chaotic-aur --install
+    echo "Repositório instalado com sucesso!"
+elif [[ $resposta == "n" || $resposta == "N" ]]; then
+    echo "O repositório chaotic-aur não foi ativado."
+else
+    echo "Opção inválida. Por favor, responda com 'y' ou 'n'."
+fi
