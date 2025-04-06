3de4dc053109fe52dfd3dbe14991cc3400d14bc1
elppans <elppans@gmail.com>
2025-04-06 00:59:14 -0300

    Atalizado script flameshot

--- pacman/pacote-pacman-flameshot.sh
+++ pacman/pacote-pacman-flameshot.sh
@@ -14,3 +14,8 @@ sudo pacman --needed --noconfirm -Syyu ""$(/usr/bin/expac -S "%o" flameshot | tr
 
 # /usr/bin/flameshot "${options[@]}"
 # ' | sudo tee /usr/local/bin/flameshot
+
+sudo cp -a /usr/share/applications/org.flameshot.Flameshot.desktop /etc/skel/.local/share/applications
+sudo sed -i 's|/usr/bin/flameshot|/usr/local/bin/flameshot|g' /etc/skel/.local/share/applications/org.flameshot.Flameshot.desktop
+sudo sed -i 's|Exec=flameshot|Exec=/usr/local/bin/flameshot|g' /etc/skel/.local/share/applications/org.flameshot.Flameshot.desktop
+cp -a /etc/skel/.local/share/applications/org.flameshot.Flameshot.desktop "$HOME/.local/share/applications"
