a672c2833bd6e33fb1917f163562dd9a5d1566c0
elppans <elppans@gmail.com>
2025-04-05 22:23:04 -0300

    Atualizado instalaçao do gnome-shell-extension manager

--- custom/gnome-shell-extensions_manager.sh
+++ custom/gnome-shell-extensions_manager.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 ## Gnome Shell Extensions Manager
-# shellcheck disable=SC2016
-echo -e 'PATH="$PATH:$HOME/.local/bin"' | tee -a "$HOME"/.bashrc
-mkdir -p "$HOME"/.local/bin
-mkdir -p "$HOME"/.config/autostart
+sudo curl -JLk -o /etc/profile.d/gnome-shell-extensions_manager.sh "https://raw.githubusercontent.com/elppans/gnome-shell-extensions_manager/refs/heads/main/gnome-shell-extensions_manager.sh"
+sudo chmod +x /etc/profile.d/gnome-shell-extensions_manager.sh
 mkdir -p "$HOME"/.local/share/gnome-shell
-wget -c "https://raw.githubusercontent.com/elppans/gnome-shell-extensions_manager/refs/heads/main/gnome-shell-extensions_manager.sh" -P "$HOME"/.local/bin
-chmod +x "$HOME"/.local/bin/gnome-shell-extensions_manager.sh
-echo -e '\n"$HOME"/.local/bin/gnome-shell-extensions_manager.sh &>>/dev/null\n' | tee -a "$HOME"/.bash_profile
 touch "$HOME"/.local/share/gnome-shell/extensions.list
 echo -e 'caffeine@patapon.info
 appindicatorsupport@rgcjonas.gmail.com
