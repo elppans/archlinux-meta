1c3185985acea12570a3e3a2794c87c7d109f0e2
elppans <elppans@gmail.com>
2025-04-05 23:57:53 -0300

    Atualizado pacote steam

--- pacman/pacote-pacman-steam_instalar.sh
+++ pacman/pacote-pacman-steam_instalar.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+# shellcheck disable=SC2015
 
-sudo pacman -S --needed steam
-sudo sed -i '/en_US.UTF-8 UTF-8/s/#//' /etc/locale.gen
-echo 'STEAM_FRAME_FORCE_CLOSE DEFAULT=1' >> ~/.pam_environment
-grep 'STEAM_FRAME_FORCE_CLOSE' /etc/environment && echo "Steam force close OK!" || echo 'STEAM_FRAME_FORCE_CLOSE=1' | sudo tee -a /etc/environment
+sudo pacman -S --needed \
+steam-native-runtime \
+xf86-video-amdgpu xf86-video-ati xf86-video-intel \
+lib32-fontconfig ttf-liberation wqy-zenhei \
+lib32-systemd
 
-mkdir -p ~/.config/autostart
-cp -a /usr/share/applications/steam.desktop ~/.config/autostart
-chmod +x ~/.config/autostart/steam.desktop
+mkdir -p "$HOME/.config/autostart/"
+cp -av /usr/share/applications/steam-native.desktop "$HOME/.config/autostart/"
+sed -i 's|Exec=/usr/bin/steam-native %U|Exec=/usr/bin/steam-native %U -silent|' "$HOME/.config/autostart/steam-native.desktop"
+
+sudo sed -i '/en_US.UTF-8 UTF-8/s///' /etc/locale.gen
+grep 'STEAM_FRAME_FORCE_CLOSE DEFAULT=1' "$HOME/.pam_environment" && echo "Steam pam_environment OK!" || echo 'STEAM_FRAME_FORCE_CLOSE DEFAULT=1' >> "$HOME/.pam_environment"
+grep 'STEAM_FRAME_FORCE_CLOSE' "/etc/environment" && echo "Steam force close OK!" || echo 'STEAM_FRAME_FORCE_CLOSE=1' | sudo tee -a "/etc/environment"
+
+
 
