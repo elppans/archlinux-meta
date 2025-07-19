0728b97cc288fbf71534948bc4ffc9705cfc90ad
elppans <elppans@gmail.com>
2025-07-19 20:58:47 -0300

    Correção linha 13

--- pacotes/flatpak.sh
+++ pacotes/flatpak.sh
@@ -10,7 +10,7 @@ fi
 # remove blacklisted packages #
 #-----------------------------#
 if [ -f "flatpak_black.list" ]; then
-    grep -v -f <(grep -v '^#' "flatpak_black.list" | sed 's/#.*//;s/ //g;/^$/d') <(sed 's/#.*//;s/\n //g;/^$/d' "flatpak.list") > "/tmp/install_flatpak_filtered.list"
+    grep -v -f <(grep -v '^#' "flatpak_black.list" | sed 's/#.*//;s/\n //g;/^$/d') <(sed 's/#.*//;s/\n //g;/^$/d' "flatpak.list") > "/tmp/install_flatpak_filtered.list"
 fi
 
 # Cria uma lista de pacotes a partir do arquivo, ignorando linhas comentadas ou vazias
