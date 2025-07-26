22067e006851392679640293159e8ac93e9a7c9a
elppans <elppans@gmail.com>
2025-07-26 00:17:39 -0300

    Criado nautilus-scripts.sh e adicionado pacotes em pacman.list

---
+++ custom/nautilus-scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Action Script, conversão de imagens
+git clone https://github.com/elppans/nautilus-scripts.git /tmp/nautilus-scripts
+cd /tmp/nautilus-scripts || exit 1
+bash ./install.sh
+cd || exit 1
