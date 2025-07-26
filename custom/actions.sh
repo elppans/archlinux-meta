b04c830c9e7fe8a85620cb3628bff91d36c13639
elppans <elppans@gmail.com>
2025-07-26 16:56:48 -0300

    Removido custom Scripts e atualizado actions.sh

--- custom/actions.sh
+++ custom/actions.sh
@@ -1,6 +1,50 @@
 #!/bin/bash
 
+
+## Action Script, Ferramentas Nautilus
+git clone https://github.com/elppans/nautilus-scripts.git /tmp/nautilus-scripts
+cd /tmp/nautilus-scripts || exit 1
+
+SCRIPT_PATH="install.sh"
+BACKUP_PATH="${SCRIPT_PATH}.bak"
+
+# Faz backup
+cp "$SCRIPT_PATH" "$BACKUP_PATH"
+
+# Usa awk para editar apenas o bloco do menu_defaults
+awk '
+BEGIN { in_block = 0; count = 0 }
+{
+    if ($0 ~ /menu_defaults=\(/) {
+        in_block = 1
+        count = 0
+    }
+    if (in_block && $0 ~ /"false"/) {
+        count++
+        if (count == 2) {  # Altere para 1 se quiser mudar o primeiro false
+            sub(/"false"/, "\"true\"")
+        }
+    }
+    if (in_block && $0 ~ /\)/) {
+        in_block = 0
+    }
+    print
+}' "$BACKUP_PATH" > "$SCRIPT_PATH"
+
+# echo "Feito! A última opção do menu_defaults foi alterada para true."
+
+
+# Fazendo instalação do Action Scripts
+bash ./install.sh
+# cd || exit 1
+
+## Action Script, Ferramentas de Imagens
+git clone https://github.com/elppans/el-images.git /tmp/el-images
+cd /tmp/el-images || exit 1
+bash ./install.sh
+
+## Action Script, Ferramentas de Diversas
 git clone https://github.com/elppans/factions-shell.git /tmp/factions-shell
 cd /tmp/factions-shell || exit 1
-./install.sh
-cd || exit 1
+bash ./install.sh
+# cd || exit 1
--- custom/el-images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## Action Script, conversão de imagens
-git clone https://github.com/elppans/el-images.git /tmp/el-images
-cd /tmp/el-images || exit 1
-./install.sh
-cd || exit 1
