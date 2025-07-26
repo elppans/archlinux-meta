b3cb905cae7a928dc755e08d7d1dbc98c6e3f312
elppans <elppans@gmail.com>
2025-07-26 15:18:10 -0300

    Adicionado path para Action Scripts

--- custom/nautilus-scripts.sh
+++ custom/nautilus-scripts.sh
@@ -3,5 +3,19 @@
 ## Action Script, conversão de imagens
 git clone https://github.com/elppans/nautilus-scripts.git /tmp/nautilus-scripts
 cd /tmp/nautilus-scripts || exit 1
+
+# Caminho para o script original
+SCRIPT_PATH="install.sh"
+
+# Faz backup antes de modificar
+cp "$SCRIPT_PATH" "${SCRIPT_PATH}.bak"
+
+# Usa sed para modificar a quinta linha dentro do bloco menu_defaults
+# Encontra o bloco e substitui apenas a última linha "false" por "true"
+sed -i '/menu_defaults=(/,/)/s/"false"/"true"/5' "$SCRIPT_PATH"
+
+echo "Modificação concluída! O arquivo original foi salvo como ${SCRIPT_PATH}.bak"
+
+# Fazendo instalação do Action Scripts
 bash ./install.sh
-cd || exit 1
+# cd || exit 1
