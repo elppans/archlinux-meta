3724ea3882b7a430686ba52e183353f38ef45c3e
elppans <elppans@gmail.com>
2026-02-25 01:56:54 -0300

    Atualização de pacotes pacman e flatpak

--- bin/autostart.sh
+++ bin/autostart.sh
@@ -9,23 +9,23 @@ AUTOSTART_DIR="$HOME/.config/autostart"
 
 # Verifica se o diretório existe
 if [ -d "$AUTOSTART_DIR" ]; then
-  # Itera por cada arquivo .desktop no diretório
-  for desktop_file in "$AUTOSTART_DIR"/*.desktop; do
-    # Verifica se o arquivo realmente existe
-    [ -e "$desktop_file" ] || continue
+	# Itera por cada arquivo .desktop no diretório
+	for desktop_file in "$AUTOSTART_DIR"/*.desktop; do
+		# Verifica se o arquivo realmente existe
+		[ -e "$desktop_file" ] || continue
 
-    # Lê a linha que começa com "Exec="
-    exec_line=$(grep '^Exec=' "$desktop_file")
+		# Lê a linha que começa com "Exec="
+		exec_line=$(grep '^Exec=' "$desktop_file")
 
-    if [ -n "$exec_line" ]; then
-      # Remove o prefixo "Exec="
-      command=${exec_line#Exec=}
+		if [ -n "$exec_line" ]; then
+			# Remove o prefixo "Exec="
+			command=${exec_line#Exec=}
 
-      # Executa o comando
-      echo "Executando: $command"
-      eval "$command" &
-    fi
-  done
+			# Executa o comando
+			echo "Executando: $command"
+			eval "$command" &
+		fi
+	done
 else
-  echo "O diretório $AUTOSTART_DIR não existe."
+	echo "O diretório $AUTOSTART_DIR não existe."
 fi
