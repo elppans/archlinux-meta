63c743a2bc44b98d91bed2d5390bb46fed58774f
elppans <elppans@gmail.com>
2026-02-26 20:33:12 -0300

    Corrigido endereço pacote hyde_bin

--- config/ml4w_hyde_bin_install.sh
+++ config/ml4w_hyde_bin_install.sh
@@ -4,4 +4,4 @@ mk4wlo="$(pwd)"
 export mk4wlo
 
 sudo pacman -Sy --needed xdg-user-dirs swappy satty
-tar -zxvf "$mk4wlo/hyde_bin.tar.gz" -C "$HOME/.config"
+tar -zxvf "$mk4wlo/hyde_bin/hyde_bin.tar.gz" -C "$HOME/.config"
