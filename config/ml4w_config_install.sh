774c5cba8ba73e498bedc0a59e67d7ea85d8aadb
elppans <elppans@gmail.com>
2026-02-26 20:51:54 -0300

    Atualizado Script

--- config/ml4w_config_install.sh
+++ config/ml4w_config_install.sh
@@ -4,7 +4,7 @@ mk4wlo="$(pwd)"
 export mk4wlo
 mkdir -p "$HOME"/.backup_dotfiles
 cp -rf "$HOME"/.config "$HOME"/.backup_dotfiles
-rsync -ah "$mk4wlo/ML4W/.config/"  "$HOME/.config/" && \
+rsync -ah "$mk4wlo/ML4W/.config/hypr/"  "$HOME/.config/" && \
 echo "Copiado .config para $HOME/.config!" || \
 echo "Não foi possível copiar .config para $HOME/.config!" && exit 1
 
