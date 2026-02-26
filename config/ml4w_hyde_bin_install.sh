eee30528158a16bddfc5b5ca76a73836dde174ce
elppans <elppans@gmail.com>
2026-02-26 20:39:44 -0300

    Atualizado Scripts

--- config/ml4w_hyde_bin_install.sh
+++ config/ml4w_hyde_bin_install.sh
@@ -4,4 +4,6 @@ mk4wlo="$(pwd)"
 export mk4wlo
 
 sudo pacman -Sy --needed xdg-user-dirs swappy satty
-tar -zxvf "$mk4wlo/hyde_bin/hyde_bin.tar.gz" -C "$HOME/.config"
+tar -zxf "$mk4wlo/hyde_bin/hyde_bin.tar.gz" -C "$HOME/.config" && \
+echo "Instalação do pacote hyde_bin finalizado!" || \
+echo "Não foi possível instalar o pacote hyde_bin!" && exit 1
