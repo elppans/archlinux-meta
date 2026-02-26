e45a7930b00774a0be8ba694cccca5f4f43a1fa6
elppans <elppans@gmail.com>
2026-02-25 23:36:18 -0300

    Corrigido diretório

--- helper/pacote-helper-yay.sh
+++ helper/pacote-helper-yay.sh
@@ -4,8 +4,8 @@ sudo pacman --needed -Sy base-devel debugedit fakeroot
 
 # Wrappers do pacman (AUR Helper)
 mkdir -p "$HOME/build" && echo 'build' >> "$HOME/.hidden"
-git clone https://aur.archlinux.org/yay.git "$HOME/.hidden/yay"
-cd "$HOME/.hidden/yay" || exit 1
+git clone https://aur.archlinux.org/yay.git "$HOME/build/yay"
+cd "$HOME/build/yay" || exit 1
 makepkg -Cris -L --needed --noconfirm
 cd - || exit 1
 
