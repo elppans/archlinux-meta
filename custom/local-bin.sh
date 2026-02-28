a0cdd1546532fd684388a535754e7d082a844250
elppans <elppans@gmail.com>
2026-02-27 23:50:37 -0300

    Atualizado local-bin.sh

--- custom/local-bin.sh
+++ custom/local-bin.sh
@@ -4,3 +4,9 @@
 # shellcheck disable=SC2154
 chmod +x "$install"/bin/*
 sudo cp -a "$install"/bin/* /usr/local/bin
+
+#
+if [ ! -f /usr/local/bin/faceconv.sh ]; then
+	sudo curl -JLk -o "/usr/local/bin/faceconv.sh" "https://raw.githubusercontent.com/elppans/sddm-silent-customizer/refs/heads/main/usr/local/bin/faceconv"
+	sudo chmod +x "/usr/local/bin/faceconv.sh"
+fi
