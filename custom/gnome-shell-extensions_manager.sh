cc2fa56ac76ad5894ed89d908f718bbbad75fc63
elppans <elppans@gmail.com>
2026-02-28 17:35:37 -0300

    Desativado Script gnome-shell-extensions_manager.sh

--- custom/gnome-shell-extensions_manager.sh
+++ custom/gnome-shell-extensions_manager.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Se ao logar em um tty, ocorrer este erro:
+# Failed to connect to GNOME Shell
+# Failed to connect to GNOME Shell
+# Failed to connect to GNOME Shell
+# Deve mover para desativar a execução do mesmo:
+# sudo mv /etc/profile.d/gnome-shell-extensions_manager.sh .
+
 ## Gnome Shell Extensions Manager
 sudo curl -JLk -o /etc/profile.d/gnome-shell-extensions_manager.sh "https://raw.githubusercontent.com/elppans/gnome-shell-extensions_manager/refs/heads/main/gnome-shell-extensions_manager.sh"
 sudo chmod +x /etc/profile.d/gnome-shell-extensions_manager.sh
