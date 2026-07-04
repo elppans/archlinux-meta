e71205303cbef9d5e36593edde45069120a068de
elppans <elppans@gmail.com>
2026-07-04 20:45:44 -0300

    Adicionado novos comando em codium-import.sh

--- bin/codium-import.sh
+++ bin/codium-import.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
     echo "VSCodium! Executando configuração..."
-    sudo chmod +x ../bin/*
-    sudo cp -a ../bin/* /usr/local/bin
-    mkdir -p "$HOME/.config/VSCodium/User"
-#   curl -JLk -o /tmp/vscodium_backup.tar.gz "https://github.com/elppans/vscodeum/raw/refs/heads/main/vscodium_backup/vscodium_backup_20250226_170128.tar.gz"
-#   tar -xzf /tmp/vscodium_backup.tar.gz -C "$HOME"/.config/VSCodium/User/
-	curl -JLk -o "$HOME"/.config/VSCodium/User/extensions_list.txt "https://github.com/elppans/vscodeum-ext/blob/main/vscodium_extensions.txt"
-    cat "$HOME"/.config/VSCodium/User/extensions_list.txt | xargs -L 1 /usr/local/bin/codium --install-extension
+    #sudo chmod +x ../bin/*
+    #sudo cp -a ../bin/* /usr/local/bin
+    #mkdir -p "$HOME/.config/VSCodium/User"
+    #curl -JLk -o /tmp/vscodium_backup.tar.gz "https://github.com/elppans/vscodeum/raw/refs/heads/main/vscodium_backup/vscodium_backup_20250226_170128.tar.gz"
+    #tar -xzf /tmp/vscodium_backup.tar.gz -C "$HOME"/.config/VSCodium/User/
+    #cat "$HOME"/.config/VSCodium/User/extensions_list.txt | xargs -L 1 /usr/local/bin/codium --install-extension
+
+	sudo rm -rf /usr/local/bin/vscodeum-extensions
+	sudo curl -JLk -o /tmp/vscodium_extensions.txt "https://raw.githubusercontent.com/elppans/vscodeum-ext/refs/heads/main/vscodium_extensions.txt"
+	sudo curl -JLk -o /usr/local/bin/vscodeum-extensions "https://raw.githubusercontent.com/elppans/vscodeum/refs/heads/main/usr/local/bin/vscodeum-extensions"
+	sudo chmod +x /usr/local/bin/vscodeum-extensions
+	sudo chmod 766 /tmp/vscodium_extensions.txt
+	/usr/local/bin/vscodeum-extensions import vscodium /tmp/vscodium_extensions.txt
