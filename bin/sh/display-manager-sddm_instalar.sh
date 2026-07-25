52232347c5c595195e3df82f92d038c219e10d5f
elppans <elppans@gmail.com>
2026-07-25 19:02:37 -0300

    Remoções de Scripts

--- bin/sh/display-manager-sddm_instalar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo pacman --needed -S sddm sddm-kcm
-# shellcheck disable=SC2046
-sudo systemctl disable $(systemctl status display-manager.service | head -n1 | awk '{print $2}')
-sudo systemctl enable sddm.service
-
--- display-manager/display-manager-sddm_instalar.sh
+++ display-manager/display-manager-sddm_instalar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2046
 
 sudo pacman -S sddm sddm-kcm
 sudo systemctl disable $(systemctl status display-manager.service | head -n1 | awk '{print $2}')
