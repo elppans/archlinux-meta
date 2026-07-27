4026a95c58c4dcfb03053ff5a994bf4c32c9ebba
elppans <elppans@gmail.com>
2026-07-26 22:32:18 -0300

    Atualizado pkgbuilds

--- custom/pkgbuilds_git.sh
+++ custom/pkgbuilds_git.sh
@@ -2,39 +2,30 @@
 
 mkdir -p "$HOME"/build
 echo -e 'build\nBuild' | tee -a "$HOME"/.hidden &>>/dev/null
+cd "$HOME/build" || exit 1
 
 # Remover pacotes virt*
 # sudo pacman -Rsunc $(pacman -Qqs | grep ^virt) ; sudo paclean ; sudo pacman -Syyu
 
-# mkdir -p "$HOME"/build/{bridge-nm,paclean,virt-qmod,virt-gmod,vscodeum}
-mkdir -p "$HOME"/build/{bridge-nm,virt-qmod,virt-gmod}
-wget -O "$HOME/build/bridge-nm/PKGBUILD" "https://raw.githubusercontent.com/elppans/bridge-nm/refs/heads/main/pkgbuild/PKGBUILD"
-#wget -O "$HOME/build/paclean/PKGBUILD" "https://raw.githubusercontent.com/elppans/paclean/refs/heads/main/PKGBUILD"
-wget -O "$HOME/build/virt-qmod/PKGBUILD" "https://raw.githubusercontent.com/elppans/virt-qmod/refs/heads/main/pkgbuild/PKGBUILD"
-wget -O "$HOME/build/virt-gmod/PKGBUILD" "https://raw.githubusercontent.com/elppans/virt-gmod/refs/heads/main/pkgbuild/PKGBUILD"
-#wget -O "$HOME/build/vscodeum/PKGBUILD" "https://raw.githubusercontent.com/elppans/vscodeum/refs/heads/main/PKGBUILD"
-cd "$HOME/build" || exit 1
-# git clone https://aur.archlinux.org/aurm.git && cd aurm || exit 1
-# makepkg -Cris
-# cd "$HOME/build" || exit 1
-# git clone https://aur.archlinux.org/pacod.git && cd pacod || exit 1
-# makepkg -Cris
-# cd "$HOME/build/bridge-nm" || exit 1
-# makepkg -Cris
-# cd "$HOME/build/paclean" || exit 1
-# makepkg -Cris
-# cd "$HOME/build/virt-qmod" || exit 1
-# makepkg -Cris
-# cd "$HOME/build/virt-gmod" || exit 1
-# makepkg -Cris
-# cd "$HOME/build/vscodeum" || exit 1
+# mkdir -p "$HOME/build/bridge-nm" && cd "$HOME/build/bridge-nm" || exit 1
+# wget -O "$HOME/build/bridge-nm/PKGBUILD" "https://raw.githubusercontent.com/elppans/bridge-nm/refs/heads/main/pkgbuild/PKGBUILD"
 # makepkg -Cris
 
-# sudo wget -O "/usr/local/bin/vscodeum-extensions" "https://raw.githubusercontent.com/elppans/vscodeum/refs/heads/main/usr/local/bin/vscodeum-extensions"
-# sudo chmod +x "/usr/local/bin/vscodeum-extensions"
+# mkdir -p "$HOME/build/virt-qmod" && cd "$HOME/build/virt-qmod" || exit 1
+# wget -O "$HOME/build/virt-qmod/PKGBUILD" "https://raw.githubusercontent.com/elppans/virt-qmod/refs/heads/main/pkgbuild/PKGBUILD"
+# makepkg -Cris
 
-# sudo wget -O "/usr/local/bin/paclean" "https://raw.githubusercontent.com/elppans/paclean/refs/heads/main/usr/bin/paclean"
-# sudo chmod +x "/usr/local/bin/paclean"
+# mkdir -p "$HOME/build/virt-gmod" && cd "$HOME/build/virt-gmod" || exit 1
+# wget -O "$HOME/build/virt-gmod/PKGBUILD" "https://raw.githubusercontent.com/elppans/virt-gmod/refs/heads/main/pkgbuild/PKGBUILD"
+# makepkg -Cris
+
+# mkdir -p "$HOME/build/vscodeum" && cd "$HOME/build/vscodeum" || exit 1
+# wget -O "$HOME/build/vscodeum/PKGBUILD" "https://raw.githubusercontent.com/elppans/vscodeum/refs/heads/main/PKGBUILD"
+# makepkg -Cris
+
+# mkdir -p "$HOME/build/faceconv" && cd "$HOME/build/faceconv" || exit 1
+# wget -c https://raw.githubusercontent.com/elppans/faceconv/refs/heads/main/pkgbuild/PKGBUILD
+# makepkg -Cris
 
 if pacman -Qqs sddm-silent-theme ; then
 	mkdir -p "$HOME/build/sddm-silent-customizer"
