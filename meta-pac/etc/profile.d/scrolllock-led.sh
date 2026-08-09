85a24c9b96124ac4f3e3228905408d591e97ff28
elppans <elppans@gmail.com>
2026-08-09 03:44:11 -0300

    Atualizado .sh de meta-pac

--- meta-pac/etc/profile.d/scrolllock-led.sh
+++ meta-pac/etc/profile.d/scrolllock-led.sh
@@ -2,6 +2,8 @@
 
 # shellcheck disable=SC2010
 for i in $(ls /sys/class/leds | grep scrolllock); do
-#echo 1 | sudo tee /sys/class/leds/$i/brightness
-brightnessctl --device="$i" set 1
+		# echo 1 | sudo tee /sys/class/leds/$i/brightness
+	if command -v brightnessctl &>/dev/null; then
+		brightnessctl --device="$i" set 1
+	fi
 done
