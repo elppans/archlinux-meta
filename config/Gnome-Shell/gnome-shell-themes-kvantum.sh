55f74471f35c9908823346f15fba514e2b33d6ea
elppans <elppans@gmail.com>
2026-08-09 13:25:02 -0300

    Alterado nomes de arquivos e temas

--- config/Gnome-Shell/gnome-shell-themes-kvantum.sh
+++ config/Gnome-Shell/gnome-shell-themes-kvantum.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-CONFIG_FILE="$HOME/.config/Kvantum/kvantum.kvconfig"
-DESIRED_THEME="KvLibadwaitaDark" # "KvLibadwaita"
+if command -v kvantummanager &>/dev/null; then
+	CONFIG_FILE="$HOME/.config/Kvantum/kvantum.kvconfig"
+	DESIRED_THEME="KvLibadwaitaDark" # "KvLibadwaita"
 
-# Garante a existência do arquivo de configuração
-mkdir -p "$(dirname "$CONFIG_FILE")"
-touch "$CONFIG_FILE"
+	# Garante a existência do arquivo de configuração
+	mkdir -p "$(dirname "$CONFIG_FILE")"
+	touch "$CONFIG_FILE"
 
-# Obtém o tema atual de forma segura
-CURRENT_THEME=$(sed -n 's/^theme=\(.*\)/\1/p' "$CONFIG_FILE")
+	# Obtém o tema atual de forma segura
+	CURRENT_THEME=$(sed -n 's/^theme=\(.*\)/\1/p' "$CONFIG_FILE")
 
-if [ "$CURRENT_THEME" != "$DESIRED_THEME" ]; then
-    echo "Configurando o tema do Kvantum para $DESIRED_THEME..."
-    
-    # Remove qualquer linha de tema duplicada e redefine sob [General]
-    sed -i '/^theme=/d' "$CONFIG_FILE"
-    if ! grep -q "^\[General\]" "$CONFIG_FILE"; then
-        echo -e "[General]\ntheme=$DESIRED_THEME" >> "$CONFIG_FILE"
-    else
-        sed -i "/^\[General\]/a theme=$DESIRED_THEME" "$CONFIG_FILE"
-    fi
-else
-    echo "Kvantum já está configurado com o tema $DESIRED_THEME."
+	if [ "$CURRENT_THEME" != "$DESIRED_THEME" ]; then
+		echo "Configurando o tema do Kvantum para $DESIRED_THEME..."
+
+		# Remove qualquer linha de tema duplicada e redefine sob [General]
+		sed -i '/^theme=/d' "$CONFIG_FILE"
+		if ! grep -q "^\[General\]" "$CONFIG_FILE"; then
+			echo -e "[General]\ntheme=$DESIRED_THEME" >>"$CONFIG_FILE"
+		else
+			sed -i "/^\[General\]/a theme=$DESIRED_THEME" "$CONFIG_FILE"
+		fi
+	else
+		echo "Kvantum já está configurado com o tema $DESIRED_THEME."
+	fi
 fi
--- config/Gnome-Shell/gnome-shell-themes.sh
+++ config/Gnome-Shell/gnome-shell-themes.sh
@@ -16,84 +16,86 @@ export GNOMESHELLDIR
 # gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' # 'prefer-light' 'default'
 # Ps.: Adicionado parâmetros na Sessão "config/Gnome-Shell/gnome-shell-set.sh"
 
-grep -q 'QT_QPA_PLATFORM="wayland;xcb"' /etc/environment || echo -e 'QT_QPA_PLATFORM="wayland;xcb"' | sudo tee -a /etc/environment
-grep -q 'QT_QPA_PLATFORMTHEME=qt5ct' /etc/environment || echo -e 'QT_QPA_PLATFORMTHEME=qt5ct' | sudo tee -a /etc/environment
-grep -q 'QT_STYLE_OVERRIDE=kvantum' /etc/environment || echo -e 'QT_STYLE_OVERRIDE=kvantum' | sudo tee -a /etc/environment
-# cat /etc/environment 
-# sudo reboot
+# KVantum
+if command -v kvantummanager &>/dev/null; then
+	grep -q 'QT_STYLE_OVERRIDE=kvantum' /etc/environment || echo -e 'QT_STYLE_OVERRIDE=kvantum' | sudo tee -a /etc/environment
 
-if [ -f "$GNOMESHELLDIR/gnome-shell-themes-kvantum.sh" ]; then
-	"$GNOMESHELLDIR/gnome-shell-themes-kvantum.sh"
-else
-	echo "Arquivo gnome-shell-themes-kvantum.sh não encontrado!"
-	exit 1
-fi
+	# if [ -f "$GNOMESHELLDIR/gnome-shell-themes-kvantum.sh" ]; then
+	# 	"$GNOMESHELLDIR/gnome-shell-themes-kvantum.sh"
+	# else
+	# 	echo "Arquivo gnome-shell-themes-kvantum.sh não encontrado!"
+	# 	exit 1
+	# fi
 
-# ==========================================
-# Temas para aplicativos Flatpak
-# ==========================================
+	# flatpak install flathub org.kde.KStyle.Kvantum//5.15
+	# flatpak install flathub org.kde.KStyle.Kvantum//6.10
+	flatpak override --user --filesystem=xdg-config/Kvantum:ro
+	flatpak override --user --env=QT_STYLE_OVERRIDE=kvantum
 
-# Instala o tema Adwaita para o runtime Qt5 e Qt6 do Flatpak
-# flatpak install flathub org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
-# flatpak install flathub org.kde.KStyle.Kvantum//5.15 
-# flatpak install flathub org.kde.KStyle.Kvantum//6.10
-# Ps.: Adicionado pacotes na Sessão "pacotes/flatpak.list"
+	# Fazer o Flatpak enxergar os temas globais do sistema
+	# flatpak override --user --filesystem=/usr/share/Kvantum:ro
 
-# Permite que os Flatpaks herdem as configurações de fontes e ícones do sistema
-flatpak override --user --filesystem=xdg-config/gtk-3.0:ro
-flatpak override --user --filesystem=xdg-config/gtk-4.0:ro
+	# Bloquear a configuração do Flatpak para enxergar os temas globais do sistema
+	# flatpak override --user --nofilesystem=/usr/share/Kvantum
 
-# Fornecer acesso aos arquivos de configuração do Kvantum/Qt5ct
-flatpak override --user --filesystem=xdg-config/Kvantum:ro
-flatpak override --user --filesystem=xdg-config/qt5ct:ro
-flatpak override --user --filesystem=xdg-config/qt6ct:ro
+	# Remover a configuração do Flatpak para enxergar os temas globais do sistema
+	# Ao rodar este comando, deve configurar novamente as variáveis e acesso do Flatpak
+	# flatpak override --user --reset --filesystem=/usr/share/Kvantum
 
-# Forçar as Variáveis de Ambiente Globais para o Flatpak
-flatpak override --user --env=QT_QPA_PLATFORMTHEME=qt5ct
-flatpak override --user --env=QT_STYLE_OVERRIDE=kvantum
+	# Tema atual do KVantum no sistema
+	KVTHEME="$(sed -n 's/^theme=\(.*\)/\1/p' "$HOME"/.config/Kvantum/kvantum.kvconfig)"
+	echo "$KVTHEME"
 
-# Fazer o Flatpak enxergar os temas globais do sistema
-# flatpak override --user --filesystem=/usr/share/Kvantum:ro
+	# Forçar o tema via variável de ambiente global no Flatpak
+	flatpak override --user --env=KVANTUM_THEME="$KVTHEME"
 
-# Bloquear a configuração do Flatpak para enxergar os temas globais do sistema
-# flatpak override --user --nofilesystem=/usr/share/Kvantum
+	# Auditar configurão do KVantum em Flatpak de dentro do sandbox
+	# flatpak run --command=cat org.kde.gwenview "$HOME"/.config/Kvantum/kvantum.kvconfig | grep -i '^theme='
 
-# Remover a configuração do Flatpak para enxergar os temas globais do sistema
-# Ao rodar este comando, deve configurar novamente as variáveis e acesso do Flatpak
-# flatpak override --user --reset --filesystem=/usr/share/Kvantum
+	# Auditar configurão do KVantum em Flatpak da variável de ambiente
+	# flatpak run --command=env org.kde.gwenview | grep -E 'QT_|KVANTUM_'
 
-# Verificação tecnica do environment via Flatpak
-# flatpak override --user --show
+	# Diretórios de temas KVantum
+	# ls /usr/share/Kvantum
 
-# Tema atual do KVantum no sistema
-KVTHEME="$(sed -n 's/^theme=\(.*\)/\1/p' "$HOME"/.config/Kvantum/kvantum.kvconfig)"
-echo "$KVTHEME"
+	# Comando para testar se o Flatpak enxerga o diretório KVantum Global
+	# flatpak run --command=ls org.kde.gwenview /usr/share/Kvantum
 
-# Forçar o tema via variável de ambiente global no Flatpak 
-flatpak override --user --env=KVANTUM_THEME="$KVTHEME"
+	# Cria o diretorio para o tema KvLibadwaita no espaço do usuário
+	mkdir -p "$HOME"/.config/Kvantum/KvLibadwaita
 
-# Auditar configurão do KVantum em Flatpak de dentro do sandbox
-# flatpak run --command=cat org.kde.gwenview "$HOME"/.config/Kvantum/kvantum.kvconfig | grep -i '^theme='
-
-# Auditar configurão do KVantum em Flatpak da variável de ambiente
-# flatpak run --command=env org.kde.gwenview | grep -E 'QT_|KVANTUM_'
-
-# Diretórios de temas KVantum
-# ls /usr/share/Kvantum
-
-# Comando para testar se o Flatpak enxerga o diretório KVantum Global
-# flatpak run --command=ls org.kde.gwenview /usr/share/Kvantum
-
-# Cria o diretorio para o tema KvLibadwaita no espaço do usuário
-mkdir -p "$HOME"/.config/Kvantum/KvLibadwaita
-
-# Copia os arquivos de definição do tema KvLibadwaita do host para o novo diretorio
-sudo curl -fsSL "https://raw.githubusercontent.com/elppans/KvLibadwaita/refs/heads/main/src/KvLibadwaita/KvLibadwaitaDark.svg" -o "/usr/share/Kvantum/KvLibadwaita/KvLibadwaitaDark.svg"
-sudo curl -fsSL "https://raw.githubusercontent.com/elppans/KvLibadwaita/refs/heads/main/src/KvLibadwaita/KvLibadwaita.svg" -o "/usr/share/Kvantum/KvLibadwaita/KvLibadwaita.svg"
-rsync -ah /usr/share/Kvantum/KvLibadwaita/ "$HOME"/.config/Kvantum/KvLibadwaita/
+	if [ -d "/usr/share/Kvantum/KvLibadwaita" ]; then
+		# Copia os arquivos de definição do tema KvLibadwaita do host para o novo diretorio
+		sudo curl -fsSL "https://raw.githubusercontent.com/elppans/KvLibadwaita/refs/heads/main/src/KvLibadwaita/KvLibadwaitaDark.svg" -o "/usr/share/Kvantum/KvLibadwaita/KvLibadwaitaDark.svg"
+		sudo curl -fsSL "https://raw.githubusercontent.com/elppans/KvLibadwaita/refs/heads/main/src/KvLibadwaita/KvLibadwaita.svg" -o "/usr/share/Kvantum/KvLibadwaita/KvLibadwaita.svg"
+		rsync -ah /usr/share/Kvantum/KvLibadwaita/ "$HOME"/.config/Kvantum/KvLibadwaita/
+	fi
 
 # Teste do Flatpak para listar o conteúdo do diretorio local mapeada dentro do sandbox
 # flatpak run --command=ls org.kde.gwenview "$HOME"/.config/Kvantum/KvLibadwaita
+fi
+
+# qt5ct/qt6ct
+if pacman -Qs '^(qt5ct|qt6ct)$' &>/dev/null; then
+	grep -q 'QT_QPA_PLATFORMTHEME=qt5ct' /etc/environment || echo -e 'QT_QPA_PLATFORMTHEME=qt5ct' | sudo tee -a /etc/environment
+	# Fornecer acesso aos arquivos de configuração do Kvantum/Qt5ct
+	flatpak override --user --filesystem=xdg-config/qt5ct:ro
+	flatpak override --user --filesystem=xdg-config/qt6ct:ro
+
+	# Forçar as Variáveis de Ambiente Globais para o Flatpak
+	flatpak override --user --env=QT_QPA_PLATFORMTHEME=qt5ct
+fi
+
+# xdg-desktop-portal-gnome xdg-desktop-portal
+if pacman -Qs '^(xdg-desktop-portal|xdg-desktop-portal-gnome)$' &>/dev/null; then
+	grep -q 'QT_QPA_PLATFORM="wayland;xcb"' /etc/environment || echo -e 'QT_QPA_PLATFORM="wayland;xcb"' | sudo tee -a /etc/environment
+	# Permite que os Flatpaks herdem as configurações de fontes e ícones do sistema
+	flatpak override --user --filesystem=xdg-config/gtk-3.0:ro
+	flatpak override --user --filesystem=xdg-config/gtk-4.0:ro
+fi
+
+# Verificação tecnica do environment via Flatpak
+# flatpak override --user --show
 
 # if [ -f "$GNOMESHELLDIR/gnome-shell-themes-flatpak.sh" ]; then
 # 	sudo cp -a "$GNOMESHELLDIR/gnome-shell-themes-flatpak.sh" /etc/profile.d/
