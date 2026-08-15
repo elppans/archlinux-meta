aa1e37d22bbb752fbe79508a74bd9621ea5dea07
elppans <elppans@gmail.com>
2026-08-15 18:38:05 -0300

    Retorno repo paru

--- helper/pacote-helper-paru.sh
+++ helper/pacote-helper-paru.sh
@@ -10,8 +10,8 @@ if pacman -Sqs | grep ^paru$ ;then
 	sudo pacman --needed --noconfirm -Sy paru
 else
 	mkdir -p "$HOME/build" && echo 'build' >>"$HOME/.hidden"
-	git clone https://aur.archlinux.org/paru-bin.git "$HOME"/build/paru-bin
-	cd "$HOME"/build/paru-bin || exit
+	git clone https://aur.archlinux.org/paru.git "$HOME"/build/paru
+	cd "$HOME"/build/paru || exit
 	makepkg --needed --noconfirm -Cris
 	cd - || exit 1
 fi
