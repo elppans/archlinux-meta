8aafaac0ea63f9ccd3475efcbc4e72c4a06f0258
elppans <elppans@gmail.com>
2026-08-15 18:15:16 -0300

    Corrigido diretório de compilação paaru

--- helper/helper_install.sh
+++ helper/helper_install.sh
@@ -36,7 +36,7 @@ if pacman -Sqs | grep ^paru$ ;then
 else
 	mkdir -p "$HOME/build" && echo 'build' >>"$HOME/.hidden"
 	git clone https://aur.archlinux.org/paru-bin.git "$HOME"/build/paru-bin
-	cd "$HOME"/build/paru || exit
+	cd "$HOME"/build/paru-bin || exit
 	makepkg --needed --noconfirm -Cris
 	cd - || exit 1
 fi
