d590b4c09cfea2d38218ba07b4d76127f2e14459
elppans <elppans@gmail.com>
2026-08-16 00:13:59 -0300

    Atualizado arquivo ml4w_config

--- config/ml4w_config_install.sh
+++ config/ml4w_config_install.sh
@@ -4,7 +4,8 @@ mk4wlo="$(pwd)"
 export mk4wlo
 mkdir -p "$HOME"/.backup_dotfiles
 rsync -ah "$HOME"/.config "$HOME"/.backup_dotfiles
-rsync -ah "$mk4wlo/ML4W/.config/"  "$HOME/.config/" && \
+# rsync -ah "$mk4wlo/ML4W/.config/"  "$HOME/.config/" && \
+rsync -ahu --backup --backup-dir="$HOME/.config/backups_$(date +%Y%m%d)" "$mk4wlo/ML4W/.config/" "$HOME/.config/" && \
 echo "Copiado .config para $HOME/.config!" || \
 echo "Não foi possível copiar .config para $HOME/.config!" && exit 1
 
