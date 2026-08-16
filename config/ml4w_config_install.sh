e60ea3b61cea67f8303de33eaee3272302d8063d
elppans <elppans@gmail.com>
2026-08-16 20:07:38 -0300

    Troca de rsync e cp por tar

--- config/ml4w_config_install.sh
+++ config/ml4w_config_install.sh
@@ -5,14 +5,23 @@ export mk4wlo
 mkdir -p "$HOME"/.backup_dotfiles
 rsync -ah "$HOME"/.config "$HOME/.backup_dotfiles/.config_$(date +%Y%m%d%H%M)"
 
-# Ps.: Para o proposito de cópia como um "/etc/skel" o rsync não serve
+# Proposito de cópia como um "/etc/skel"
+
 # Cenário 1: "Apenas adicione o que NÃO existe" (Ignorar completamente o que já está no destino)
 # rsync -ahvz --keep-dirlinks --copy-unsafe-links --ignore-existing "$mk4wlo/ML4W/.config/." "$HOME/.config/" && export RCP="1"
-cp -aL -n "$mk4wlo/ML4W/." "$HOME/" && export RCP="1"
+# cp -aL -n "$mk4wlo/ML4W/." "$HOME/" && export RCP="1"
 
 # Cenário 2: "Adicione o que não existe OU atualize apenas se for DIFERENTE"
 # rsync -ahvz --keep-dirlinks --copy-unsafe-links "$mk4wlo/ML4W/.config/." "$HOME/.config/" && RCP="1"
 # cp -aL -u "$mk4wlo/ML4W/.config/." "$HOME/.config/" && RCP="1"
+
+# Usando tar:
+# -k (--keep-old-files): 
+# Trata a presença de um arquivo no destino como um erro de extração, emitindo o aviso de falha e retornando status code 2 (erro não fatal no término).
+# --skip-old-files: 
+# Ignora silenciosamente qualquer arquivo que já exista no destino, sem emitir mensagens no stderr e terminando a execução com código de saída limpo (0).
+
+tar -c -C "$mk4wlo/ML4W" . | tar -x --skip-old-files -f - -C "$HOME"
 
 	if [ "$RCP" == "1" ]; then
 		echo "Copiado .config para $HOME/.config!"
