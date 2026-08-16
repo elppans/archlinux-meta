313fac87082a1b7bdfdca589dc25b3a0b532e670
elppans <elppans@gmail.com>
2026-08-16 19:38:52 -0300

    Troca de rsync por cp

--- config/ml4w_config_install.sh
+++ config/ml4w_config_install.sh
@@ -5,11 +5,14 @@ export mk4wlo
 mkdir -p "$HOME"/.backup_dotfiles
 rsync -ah "$HOME"/.config "$HOME/.backup_dotfiles/.config_$(date +%Y%m%d%H%M)"
 
+# Ps.: Para o proposito de cópia como um "/etc/skel" o rsync não serve
 # Cenário 1: "Apenas adicione o que NÃO existe" (Ignorar completamente o que já está no destino)
-rsync -ahvz --keep-dirlinks --copy-unsafe-links --ignore-existing "$mk4wlo/ML4W/.config/." "$HOME/.config/" && export RCP="1"
+# rsync -ahvz --keep-dirlinks --copy-unsafe-links --ignore-existing "$mk4wlo/ML4W/.config/." "$HOME/.config/" && export RCP="1"
+cp -aL -n "$mk4wlo/ML4W/." "$HOME/" && export RCP="1"
 
 # Cenário 2: "Adicione o que não existe OU atualize apenas se for DIFERENTE"
 # rsync -ahvz --keep-dirlinks --copy-unsafe-links "$mk4wlo/ML4W/.config/." "$HOME/.config/" && RCP="1"
+# cp -aL -u "$mk4wlo/ML4W/.config/." "$HOME/.config/" && RCP="1"
 
 	if [ "$RCP" == "1" ]; then
 		echo "Copiado .config para $HOME/.config!"
