4bf1e4a4bc2e3547bfe1191cd8063a1aa950d153
elppans <elppans@gmail.com>
2026-08-23 04:42:05 -0300

    Atualizado comentario de comandos custom

--- hyprland-with_ML4W_theme.sh
+++ hyprland-with_ML4W_theme.sh
@@ -253,6 +253,6 @@ exit 0
 # tmpdir=$(mktemp -d)
 # mkdir -p "$tmpdir/custom"
 # curl -sSL https://github.com/elppans/archlinux-meta/archive/refs/heads/main.tar.gz | tar -xz -C "$tmpdir/custom" --strip-components=2 archlinux-meta-main/custom
-# rm -f "$tmpdir/custom/{shell-minimalista.sh,bin.sh}"
+# rm -f "$tmpdir/custom"/{shell-minimalista.sh,bin.sh}
 # find "$tmpdir/custom" -type f -name "*.sh" -executable -exec {} \;
 # rm -rf "$tmpdir"
