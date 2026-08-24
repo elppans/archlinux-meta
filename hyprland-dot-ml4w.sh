ffee1fe90049d9c1c379cc7893e1fa1929e86e3c
elppans <elppans@gmail.com>
2026-08-23 21:14:18 -0300

    Adicionado tty

--- hyprland-dot-ml4w.sh
+++ hyprland-dot-ml4w.sh
@@ -152,7 +152,7 @@ ml4w_lista_de_dependências_oficiais() {
 ml4w_os_install() {
 	echo "Instalando ML4W..."
 	sleep 5
-	bash -c "$(curl -fsSL https://ml4w.com/os/stable)"
+	bash -c "$(curl -fsSL https://ml4w.com/os/stable)" </dev/tty
 }
 ml4w_configuracoes_customizadas() {
 	# **CUSTOMIZAÇÃO**
