1b83cb7ba13761d684046aabeafcfad3acf2390f
elppans <elppans@gmail.com>
2026-09-06 05:57:06 -0300

    Adicionado fechamento forçado do nautilus

--- custom/git_nautilus-scripts.sh
+++ custom/git_nautilus-scripts.sh
@@ -26,7 +26,9 @@ bash ./install.sh
 ###
 if [ "$(command -v nautilus)" ]; then
 	mkdir -p "$HOME"/.local/share/nautilus/scripts
-	nautilus -q
+	nautilus -q &
+	sleep 5
+	pkill -9 nautilus
 	nautilus-scripts
 	el-images
 	factions-shell
