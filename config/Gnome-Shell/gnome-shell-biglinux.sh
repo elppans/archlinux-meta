93af5b1956be656ee1bd9e33f492238dea0ed0d3
elppans <elppans@gmail.com>
2026-09-09 02:14:51 -0300

    Criado gnome-shell-biglinux

---
+++ config/Gnome-Shell/gnome-shell-biglinux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+makebuild() {
+# --[no]removemake       Remove dependências de compilação após instalação
+# --[no]sudoloop         Repete chamadas sudo em segundo plano para evitar esgotar o tempo
+# --[no]keepsrc          Mantém diretórios src/ e pkg/ após compilar pacotes
+# yay/paru {-B --build}       [diretório(s)]
+
+# -C, --cleanbuild Remove o diretório $srcdir/ antes de compilar o pacote
+# -r, --rmdeps     Remove dependências instaladas após uma compilação bem-sucedida
+# -i, --install    Instala pacote após empacotamento bem-sucedido
+# -s, --syncdeps   Instala dependências em falta com pacman
+
+if [ "$(command -v yay)" ]; then
+	yay --needed --noconfirm --removemake --sudoloop --build "$(pwd)"
+elif [ "$(command -v paru)" ]; then
+	paru --needed --noconfirm --removemake --sudoloop --nokeepsrc --build "$(pwd)"
+else
+	makepkg --needed --noconfirm -Cris
+fi
+}
+
+mkdir -p "$HOME/build" || exit 1
+git clone https://github.com/biglinux/big-hardware-info
+cd big-hardware-info/pkgbuild || exit 1
+makebuild
+git clone https://github.com/big-comm/gnome-shell-big-shot
+cd big-hardware-info/pkgbuild || exit 1
+makebuild
+git clone https://github.com/big-comm/big-gnome-center
+cd big-hardware-info/pkgbuild || exit 1
+makebuild
