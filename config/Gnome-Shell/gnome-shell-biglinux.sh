5d37f1a6166a74733cadf3f697c3228881f7600c
elppans <elppans@gmail.com>
2026-09-22 20:42:34 -0300

    Atualizado make_timestamp

--- config/Gnome-Shell/gnome-shell-biglinux.sh
+++ config/Gnome-Shell/gnome-shell-biglinux.sh
@@ -7,6 +7,7 @@ if grep -q '^pkgrel=.*date' PKGBUILD; then
 fi
 }
 makebuild() {
+make_timestamp
 # --[no]removemake       Remove depend\C3\AAncias de compila\C3\A7\C3\A3o ap\C3\B3s instala\C3\A7\C3\A3o
 # --[no]sudoloop         Repete chamadas sudo em segundo plano para evitar esgotar o tempo
 # --[no]keepsrc          Mant\C3\A9m diret\C3\B3rios src/ e pkg/ ap\C3\B3s compilar pacotes
@@ -37,40 +38,34 @@ mkdir -p "$HOME/build"
 # cd "$HOME/build" || exit 1
 # git clone https://github.com/big-comm/gnome-shell-extension-copyous.git
 # cd gnome-shell-extension-copyous/pkgbuild || exit 1
-# make_timestamp
 # echo '^4' | makebuild
 
 # Copyous AUR - Esta vers\C3\A3o funciona perfeitamente - Necess\C3\A1rio para BIG Gnome Center
 cd "$HOME/build" || exit 1
 git clone https://aur.archlinux.org/gnome-shell-extension-copyous-bin.git
 cd gnome-shell-extension-copyous-bin || exit 1
-make_timestamp
 echo '^4' | makebuild
 
 cd "$HOME/build" || exit 1
 git clone https://github.com/biglinux/big-hardware-info
 cd big-hardware-info/pkgbuild || exit 1
-make_timestamp
 makebuild
 
 cd "$HOME/build" || exit 1
 git clone https://github.com/big-comm/gnome-shell-big-shot
 cd gnome-shell-big-shot/pkgbuild || exit 1
-make_timestamp
 makebuild
 
 # O aplicativo funciona, mas para fazer algumas altera\C3\A7\C3\B5es, \C3\A9 necess\C3\A1rio a instala\C3\A7\C3\A3o de outros pacotes do BIGLinux
 # cd "$HOME/build" || exit 1
 # git clone https://github.com/big-comm/biglinux-settings.git
 # cd biglinux-settings/pkgbuild || exit 1
-# make_timestamp
 # makebuild
 
 # big-bibata-cursor-theme - Dependência para "big-gnome-center"
 cd "$HOME/build" || exit 1
 git clone -b dev-talesam https://github.com/elppans/Bibata_Cursor.git
 cd Bibata_Cursor/pkgbuild || exit 1
-make_timestamp
 makebuild
 
 # Aplicativo de temas, ele \C3\A9 muito bom
@@ -79,32 +74,27 @@ git clone -b dev-talesam https://github.com/elppans/big-gnome-center.git
 #grep -iE '(fill|stroke)=' distributor-logo-blackarch.svg
 #sed -i 's/fill="rgb(30.196078%, 30.196078%, 30.196078%)"/fill="#808080"/g' distributor-logo-blackarch.svg
 cd big-gnome-center/pkgbuild || exit 1
-make_timestamp
 makebuild
 
 # Wallpapers para acompanhar o Big Gnome Center
 cd "$HOME/build" || exit 1
 git clone https://github.com/big-comm/comm-wallpapers-gnome.git
 cd comm-wallpapers-gnome/pkgbuild || exit 1
-make_timestamp
 makebuild
 
 # Icones necessario para  o Big Gnome Center
 cd "$HOME/build" || exit 1
 git clone https://github.com/biglinux/bigicons-papient.git
 cd bigicons-papient/pkgbuild || exit 1
-make_timestamp
 makebuild
 
 # O aplicativo funciona, mas para fazer algumas altera\C3\A7\C3\B5es, \C3\A9 necess\C3\A1rio a instala\C3\A7\C3\A3o de outros pacotes do BIGLinux/Manjaro
 # cd "$HOME/build" || exit 1
 # git clone https://github.com/biglinux/biglinux-driver-manager.git
 # cd biglinux-driver-manager/pkgbuild || exit 1
-# make_timestamp
 # makebuild
 
 # cd "$HOME/build" || exit 1
 # git clone https://github.com/biglinux/big-kernel-manager.git
 # cd big-kernel-manager/pkgbuild || exit 1
-# make_timestamp
 # makebuild
